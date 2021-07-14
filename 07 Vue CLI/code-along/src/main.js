54b8e57168cbadf6c2894fee20e52559fbdb4776
Paricia Muñoz <patrimdm96@gmail.com>
2021-07-14 10:17:54 +0100

    chapter 7 complete + components and props 101

--- 07 Vue CLI/code-along/src/main.js
+++ 07 Vue CLI/code-along/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+//This configuration makes the component available globally
+
+/* import FriendContact from './components/FriendContact.vue'
+
+const app = createApp(App)
+app.component('friend-contact', FriendContact)
+app.mount('#app') */
+
+
 createApp(App).mount('#app')
