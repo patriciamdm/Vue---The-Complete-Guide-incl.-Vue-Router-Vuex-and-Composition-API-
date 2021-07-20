4846c078bff76c03d7e10417b58a6cd2c1ad74e8
Paricia Muñoz <patrimdm96@gmail.com>
2021-07-20 12:39:50 +0100

    basic routing2

---
+++ 16 Prj - Find a Coach/codealong-project/src/pages/coaches/CoachDetails.vue
@@ -0,0 +1,5 @@
+<template>
+   details for coach
+   <router-link to="/coaches/id/contact">contact</router-link>
+   <router-view></router-view>
+</template>
---
+++ 16 Prj - Find a Coach/codealong-project/src/pages/coaches/CoachesList.vue
@@ -0,0 +1,27 @@
+<template>
+   <section>
+      filter
+   </section>
+   <section>
+      <div class="controls">
+         <button>Refresh coaches</button>
+         <router-link to="/register">Register as coach</router-link>
+      </div>
+      <ul>list</ul>
+   </section>
+</template>
+
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
