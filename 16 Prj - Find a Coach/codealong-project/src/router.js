5bbc63826d0f976cded0ed2b8f9b1d871d9a5968
Paricia Muñoz <patrimdm96@gmail.com>
2021-07-20 12:15:58 +0100

    codealong project 16 setup

---
+++ 16 Prj - Find a Coach/codealong-project/src/router.js
@@ -0,0 +1,24 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue'
+import CoachDetails from './pages/coaches/CoachDetails.vue'
+import NewCoach from './pages/coaches/NewCoach.vue'
+import ContactCoach from './pages/requests/ContactCoach.vue'
+import RequestsList from './pages/requests/RequestsList.vue'
+import NotFound from './pages/NotFound.vue'
+
+const router = createRouter({
+   history: createWebHistory(),
+   routes: [
+      { path: '/', redirect: '/coaches' },
+      { path: '/coaches', component: CoachesList },
+      { path: '/coaches/:id', component: CoachDetails, children: [
+         { path: 'contact', component: ContactCoach }]
+      },
+      { path: '/register', component: NewCoach },
+      { path: '/requests', component: RequestsList },
+      { path: '/:notFound(.*)', component: NotFound }
+   ]
+});
+
+export default router;
