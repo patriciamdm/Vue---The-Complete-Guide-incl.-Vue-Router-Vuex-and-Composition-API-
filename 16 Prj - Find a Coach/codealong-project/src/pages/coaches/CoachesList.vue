25592799efde94063455394f12be7aaeeb23961a
Paricia Muñoz <patrimdm96@gmail.com>
2021-07-21 18:39:46 +0100

    coaches list filtering

--- 16 Prj - Find a Coach/codealong-project/src/pages/coaches/CoachesList.vue
+++ 16 Prj - Find a Coach/codealong-project/src/pages/coaches/CoachesList.vue
@@ -1,17 +1,17 @@
 <template>
    <section>
-      filter
+      <filter-list @change-filter="setFilters"></filter-list>
    </section>
    <section>
       <base-card>
-      <div class="controls">
-         <base-button mode="outline">Refresh coaches</base-button>
-         <base-button link to="/register">Register as Coach</base-button>
-      </div>
-      <ul v-if="hasCoaches">
-         <CoachItem v-for="elm in filteredCoaches" :key="elm.id" :coach="elm" />
-      </ul>
-      <h3 v-else>No coaches found.</h3>
+         <div class="controls">
+            <base-button mode="outline">Refresh coaches</base-button>
+            <base-button link to="/register">Register as Coach</base-button>
+         </div>
+         <ul v-if="hasCoaches">
+            <CoachItem v-for="elm in filteredCoaches" :key="elm.id" :coach="elm" />
+         </ul>
+         <h3 v-else>No coaches found.</h3>
       </base-card>
    </section>
 </template>
@@ -19,15 +19,31 @@
 
 <script>
 import CoachItem from '../../components/coaches/CoachItem.vue'
-import BaseButton from '../../components/ui/BaseButton.vue'
+import FilterList from '../../components/coaches/FilterList.vue'
 
 export default {
-   components: { CoachItem, BaseButton },
+   components: { CoachItem, FilterList },
+   data: () => ({
+      activeFilters: {
+         frontend: true,
+         backend: true,
+         career: true
+      }
+   }),
    computed: {
       filteredCoaches() {
-         return this.$store.getters['coaches/coaches']
+         const coaches = this.$store.getters['coaches/coaches']
+         return coaches.filter(elm => {
+            if (this.activeFilters.frontend && elm.areas.includes('frontend')) return true;
+            if (this.activeFilters.backend && elm.areas.includes('backend')) return true;
+            if (this.activeFilters.career && elm.areas.includes('career')) return true;
+            return false;
+         })
       },
       hasCoaches() { return this.$store.getters['coaches/hasCoaches'] }
+   },
+   methods: {
+      setFilters(updatedFilters) { this.activeFilters = updatedFilters }
    }
 }
 </script>
