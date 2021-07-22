29c22abd99e6917bae39ffec8e36046a10764d9d
Paricia Muñoz <patrimdm96@gmail.com>
2021-07-22 10:40:45 +0100

    not found page

---
+++ 16 Prj - Find a Coach/codealong-project/src/pages/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+   <section>
+      <base-card>
+      <h2>Page not found.</h2>
+      <p>This page could not be found. Please check our other pages.</p>
+      </base-card>
+   </section>
+</template>
